ef757873b70bf4977033c04a6f4a34b8bdff95dc
dubey <thanosuniverse143@gmail.com>
2024-03-25 11:37:44 +0530

    this is my first commit

---
+++ test.py
@@ -0,0 +1,13 @@
+import pymongo
+client = pymongo.MongoClient("mongodb+srv://chirag04:up65dp2788@cluster1.z0xulk8.mongodb.net/?retryWrites=true&w=majority&appName=Cluster1")
+db = client.test
+print(db)
+
+d = {
+    "name" : "aditya",
+    "email" : "aditya@ineuron.ai",
+    "surname" : "dubey"
+}
+db1 = client['mongotest']
+coll = db1['test']
+coll.insert_one(d)
